0a5ee3af5a170b5aefe34cab050898646baf68e3
Gokulakrishnan <sgokulcse10@gmail.com>
2022-09-08 22:42:48 +0530

    added and configured fonts

--- App/theme/typography.tsx
+++ App/theme/typography.tsx
@@ -1,7 +1,17 @@
 import { Platform } from "react-native";
 export const typography = {
-    primary: "sans-serif",
-    primary_light: "sans-serif-light",
-    primary_thin: "sans-serif-thin",
-    primary_medium: "sans_serif_medium",
-} 
+
+    RobotoBold: 'Roboto-Bold',
+    RobotoLight: 'Roboto-Light',
+    RobotoMedium: 'Roboto-Medium',
+    RobotoRegular: 'Roboto-Regular',
+    RobotoThin: 'Roboto-Thin',
+};
+export const fontSize = {
+    s: 14,
+    xs: 12,
+    l: 16,
+    xl: 18,
+    xxl: 20,
+    xxxl: 22
+}
